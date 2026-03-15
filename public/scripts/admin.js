4def674b381574462259d448c2cfc5f1410a9893
Jesse Smrekar <jjsmre@gmail.com>
2026-03-15 11:48:28 -0500

    update admin page

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -20,13 +20,21 @@ function getUsers() {
 
         for (inc in data) {
             var entry = data[inc];
+
+            var timeToDisplay = new Date(entry.NEXT_MUSIC_QUEUE_DATE);
+            if (timeToDisplay > new Date("2030-01-01") || timeToDisplay < new Date()) {
+                timeToDisplay = "now";
+            } else {
+                timeToDisplay = new Date(entry.NEXT_MUSIC_QUEUE_DATE).toLocaleTimeString();
+            }
+
+
             userTable.innerHTML += 
             '<tr id="search-result-' + inc +'">' +
             // '<div id="search-result-' + inc +'" onclick=javascript:makeSelection(' + inc + ')>' +
-            '<td>' + entry.id + '</td>' +
-            '<td>' + entry.firstName + '</td>' +
-            '<td>' + entry.lastName + '</td>' +
-            '<td>' + entry.nextQueueTime + '</td>' +
+            '<td>' + entry.FIRST_NAME + '</td>' +
+            '<td>' + entry.LAST_NAME + '</td>' +
+            '<td>' + timeToDisplay + '</td>' +
             '</tr>';
         }
     })
